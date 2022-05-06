2c107a8548d217064a1e31c1b1a4b714c009b7cc
Stanley <we.byte.together@gmail.com>
2022-05-05 18:19:46 -0700

    updated backend API address

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "http://54.177.162.207:5000/api/";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
